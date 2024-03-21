4f01cc83309f5a7959b176c38d099b3f5de0ac06
Sonam Datok <datoksonam@gmail.com>
2024-03-21 19:16:46 -0400

    Migrate font to Mona Sans and mock LandingPage.tsx revamp

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,12 +1,18 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
     return (
-        <div className="mt-20">
-            <h1 className="title">E-BIKE EVERYWHERE</h1>
-            <Link to="/bikes" className="block mt-4 text-lg md:text-xl font-sans">View Bike Sharing Data</Link>
-            <Link to="/map" className="block mt-4 text-lg md:text-xl font-sans">View Map</Link>
-            <Link to="/about" className="block mt-4 text-lg md:text-xl font-sans">About Us</Link>
+        <div className="mt-10 text-center">
+            <h1 className="title text-5xl md:text-6xl lg:text-7xl xl:text-8xl">E-BIKE EVERYWHERE</h1>
+            <p className="pt-6 italic mt-4 text-lg md:text-xl font-bold">
+                your official guide to navigating Toronto with {/* Fixed spacing */}
+                <a href="https://bikesharetoronto.com/" className="no-underline hover:no-underline text-inherit ml-1">BIKESHARE</a>
+            </p>
+            {/* Button to redirect to MapView */}
+            <Link to="/map" className="mt-12 inline-block bg-[#6638f0] hover:bg-[#df68b4] text-white hover:text-black font-bold py-4 px-6 rounded-full text-lg md:text-xl transition duration-300 ease-in-out">
+                Find E-Bikes
+            </Link>
         </div>
     );
 };
