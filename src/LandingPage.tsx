ddcd9dc012cd7f6f696bf9d99b537460d817b1a7
Sonam Datok <datoksonam@gmail.com>
2024-03-21 18:13:52 -0400

    Merge CSS files and import Mona Sans font family

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 const LandingPage = () => {
     return (
         <div className="mt-20">
-            <h1>E-BIKE EVERYWHERE</h1>
-            <Link to="/bikes">View Bike Sharing Data</Link>
-            <Link to="/map">View Map</Link>
-            <Link to="/about">About Us</Link>
+            <h1 className="title">E-BIKE EVERYWHERE</h1>
+            <Link to="/bikes" className="block mt-4 text-lg md:text-xl font-sans">View Bike Sharing Data</Link>
+            <Link to="/map" className="block mt-4 text-lg md:text-xl font-sans">View Map</Link>
+            <Link to="/about" className="block mt-4 text-lg md:text-xl font-sans">About Us</Link>
         </div>
     );
 };
